a747a241f2005fecdae96cee830a2a262d34d9bd
Alex Amedia <aamedia@asu.edu>
2025-03-19 15:58:54 -0700

    Added themes, time series styling

--- client/src/features/timeSeries/dashboard/TimeSeriesCard.tsx
+++ client/src/features/timeSeries/dashboard/TimeSeriesCard.tsx
@@ -1,45 +1,78 @@
-import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Typography, useTheme } from "@mui/material";
 import { TimeSeries } from "../../../lib/types";
-import { useTimeSeries } from "../../../lib/hooks/useTimeSeries";
 import { Link } from "react-router";
+import { BaseSyntheticEvent } from "react";
+import { AccessTime, Place } from "@mui/icons-material";
+import { tokens } from "../../../theme";
 
 type Props = {
     timeSeries: TimeSeries
 }
 
 const TimeSeriesCard = ({ timeSeries }: Props) => {
-  const {deleteUserTimeSeries} = useTimeSeries();
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode)
+
+  const handleImageError = (e: BaseSyntheticEvent) => {
+    console.log(e)
+    e.target.onerror = null;
+    // e.target.style.display = 'none'
+    e.target.src = "https://placehold.co/400?text=.png"
+}
 
   return (
-    <Card sx={{borderRadius: 3}}>
-        <CardContent>
-            <Typography variant='h5'>{timeSeries["Meta Data"]["2. Symbol"]}</Typography>
-            <Typography sx={{color: 'text.secondary', mb: 1}}>{timeSeries["Meta Data"]["3. Last Refreshed"]}</Typography>
-            <Typography variant='body2'>{timeSeries["Meta Data"]["5. Time Zone"]}</Typography>
-            <Typography variant='subtitle1'>{timeSeries.id}</Typography>
-        </CardContent>
-        <CardActions sx={{display: 'flex', justifyContent:'space-between', pb:2}}>
-            <Chip label={timeSeries["Meta Data"]["4. Output Size"]} />
-            <Box display={'flex'} gap={3}>
-              <Button 
-                component={Link}
-                to={`/timeSeries/${timeSeries.id}`}
-                size='medium'
-                variant="contained"
-                color='inherit'
-              >
-                View
-              </Button>
-              <Button 
-                onClick={() => deleteUserTimeSeries.mutate(timeSeries.id)} 
-                size='medium' 
-                variant='contained' 
-                color="inherit"
-              >
-                Delete
-              </Button>
+    <Card elevation={3} sx={{borderRadius: 3}}>
+      <Box display='flex' alignItems='center' justifyContent='space-between'>
+        <CardHeader
+          title={timeSeries["Meta Data"]["2. Symbol"]}
+          titleTypographyProps={{
+            fontWeight: 'bold',
+            fontSize: 20
+          }}
+          subheader={
+            <CardMedia
+              component="img"
+              sx={{ display: "flex", maxWidth: 50, marginLeft: '9px'}} 
+              height="50"
+              src={`https://eodhd.com/img/logos/US/${timeSeries["Meta Data"]["2. Symbol"]}.png`}
+              onError={handleImageError}
+            >
+          </CardMedia>
+          }
+        />
+      </Box>
+      <Divider sx={{bgcolor: colors.blueAccent[400]}}/>
+      <CardContent >
+        <Box display='flex' sx={{minHeight: '250px'}}>
+          <Box display='flex' flexDirection='column' mb={2} px={2}>
+            <Box display='flex'>
+              <AccessTime sx={{mr: 1}}/>
+              <Typography variant='body2'>{timeSeries["Meta Data"]["3. Last Refreshed"]}</Typography>
             </Box>
-        </CardActions>
+            <Box display='flex'>
+              <Place sx={{mr: 1}}/>
+              <Typography variant='body2'>{timeSeries["Meta Data"]["5. Time Zone"]}</Typography>
+            </Box>
+          </Box>
+          <Box height='100' width='100%' border='2px dotted' borderColor={colors.blueAccent[400]}>
+
+          </Box>
+        </Box>
+      </CardContent>
+      <Divider sx={{mb: 3, bgcolor: colors.blueAccent[400]}}/>
+      <CardActions sx={{display: 'flex', justifyContent: 'end', pb:2, pr: 2}}>
+          <Box display={'flex'} gap={3}>
+            <Button 
+              component={Link}
+              to={`/timeSeries/${timeSeries.id}`}
+              size='medium'
+              variant="contained"
+              color='inherit'
+            >
+              View
+            </Button>
+          </Box>
+      </CardActions>
     </Card>
   )
 }
