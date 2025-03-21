466eafefafe37ee7457c8fa4f4b740330715397e
Alex Amedia <aamedia@asu.edu>
2025-03-21 12:54:59 -0700

    Added candlestick chart

--- client/src/features/timeSeries/chart/TimeSeriesChart.tsx
+++ client/src/features/timeSeries/chart/TimeSeriesChart.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useRef } from 'react';
-import { createChart, ColorType, AreaSeries } from 'lightweight-charts';
+import { createChart, ColorType, CandlestickSeries } from 'lightweight-charts';
 import { useTheme } from '@mui/material';
 import { tokens } from '../../../lib/themes/theme';
-import TimeSeriesData from './TimeSeriesData';
+import { TimeSeriesDaily } from '../../../lib/types';
 
-const TimeSeriesChart = () => {
+type Props = {
+    timeseriesDaily: TimeSeriesDaily
+}
+
+const TimeSeriesChart = ({timeseriesDaily}: Props) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const chartContainerRef = useRef<HTMLDivElement | null>(null);
@@ -16,25 +20,38 @@ const TimeSeriesChart = () => {
     
         const chart = createChart(chartContainerRef.current, {
             layout: {
+                textColor: colors.primary[200],
                 background: { type: ColorType.Solid, color: colors.primary[900] },
             },
             width: chartContainerRef.current.clientWidth,
             height: chartContainerRef.current.clientHeight,
         });
-    
-        const newSeries = chart.addSeries(AreaSeries, {
-            lineColor: colors.primary[100],
-            topColor: colors.blueAccent[900],
-            bottomColor: colors.blueAccent[100],
+
+        const candlestickSeries = chart.addSeries(CandlestickSeries, {
+            upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
+            wickUpColor: '#26a69a', wickDownColor: '#ef5350',
         });
-    
-        newSeries.setData(TimeSeriesData());
+
+        //TODO: Refactor types so that this conversion can be avoided
+        const flattenedList = Object.entries(timeseriesDaily).map(([date, data]) => ({
+            time: date,
+            open: Number(data['1. open']),
+            high: Number(data['2. high']),
+            low: Number(data['3. low']),
+            close: Number(data['4. close'])
+        }))
+        .sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime());
+
+        candlestickSeries.setData(flattenedList)
+
+        //TODO: Change to appropriate time scale
+        // chart.timeScale().fitContent();
     
         return () => {
             chart.remove();
         };
 
-    }, [theme.palette.mode, colors]);
+    }, [theme.palette.mode, colors, timeseriesDaily]);
 
     return  <div ref={chartContainerRef} style={{ width: '100%', height: '200px' }} />;
 }
--- client/src/features/timeSeries/dashboard/TimeSeriesLeaderboard.tsx
+++ client/src/features/timeSeries/dashboard/TimeSeriesLeaderboard.tsx
@@ -13,7 +13,7 @@ export default function TimeSeriesLeaderboard() {
                 }}
             >
                 <Typography variant="h6">
-                    Leaderboard: 8 People Playing
+                    Leaderboard: 9 People Playing
                 </Typography>
             </Paper>
             <Paper sx={{ padding: 2 }}>
@@ -25,11 +25,12 @@ export default function TimeSeriesLeaderboard() {
                                     <Typography variant="h6">1. Cassidy</Typography>
                                     <Typography variant="h6">2. Janet</Typography>
                                     <Typography variant="h6">3. Trevor</Typography>
-                                    <Typography variant="h6">4. Alex</Typography>
-                                    <Typography variant="h6">5. Bill</Typography>
-                                    <Typography variant="h6">6. Chris</Typography>
-                                    <Typography variant="h6">7. Moinca</Typography>
-                                    <Typography variant="h6">8. Cletus</Typography>
+                                    <Typography variant="h6">4. Dave</Typography>
+                                    <Typography variant="h6">5. Alex</Typography>
+                                    <Typography variant="h6">6. Bill</Typography>
+                                    <Typography variant="h6">7. Chris</Typography>
+                                    <Typography variant="h6">8. Monica</Typography>
+                                    <Typography variant="h6">9. Cletus</Typography>
                                 </ListItemText>
                             </ListItem>
                         </List>
