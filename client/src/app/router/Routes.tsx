d95b98cc043df214e77f3e2a292e5bc32bda6469
Alex Amedia <aamedia@asu.edu>
2025-03-28 10:19:25 -0700

    Client error handling

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,9 +1,12 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import TimeSeriesDashboard from "../../features/timeSeries/dashboard/TimeSeriesDashboard";
 import Counter from "../../features/counter/Counter";
 import TimeSeriesTrade from "../../features/form/TimeSeriesTrade";
+import TestErrors from "../../features/errors/TestErrors";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 export const router = createBrowserRouter([
     {
@@ -14,7 +17,12 @@ export const router = createBrowserRouter([
             {path: 'timeseries', element: <TimeSeriesDashboard />},
             {path: 'trade/:ticker', element: <TimeSeriesTrade />},
             {path: 'trade', element: <TimeSeriesTrade key='create'/>},
-            {path: 'counter', element: <Counter/>},
+            {path: 'counter', element: <Counter />},
+            {path: 'not-found', element: <NotFound />},
+            {path: 'errors', element: <TestErrors />},
+            {path: 'server-error', element: <ServerError />},
+            {path: '*', element: <Navigate replace to='/not-found' />}
+
         ]
     }
 ])
